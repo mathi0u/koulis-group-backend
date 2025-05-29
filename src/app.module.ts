d78dec35bc66b848fb3471796bc23a0b625e4b13
mathi0u <>
2025-05-29 01:36:44 +0000

    feat: add additional modules to AppModule imports for enhanced functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,30 @@ import { Module } from '@nestjs/common'
 import { ConfigsModule } from './configs/configs.module'
 import { UsersModule } from './users/users.module'
 import { HealthModule } from './health/health.module'
+import { SucribtionsModule } from './sucribtions/sucribtions.module'
+import { ClientsModule } from './clients/clients.module'
+import { PaymentsModule } from './payments/payments.module'
+import { ProductsModule } from './products/products.module'
+import { AccountingModule } from './accounting/accounting.module'
+import { ServicesModule } from './services/services.module'
+import { ProgramsModule } from './programs/programs.module'
+import { InventoryModule } from './inventory/inventory.module'
 
 @Module({
-    imports: [ConfigsModule, UsersModule, HealthModule, TypeOrmModule.forFeature([])],
+    imports: [
+        ConfigsModule,
+        UsersModule,
+        HealthModule,
+        TypeOrmModule.forFeature([]),
+        SucribtionsModule,
+        ClientsModule,
+        PaymentsModule,
+        ProductsModule,
+        AccountingModule,
+        ServicesModule,
+        ProgramsModule,
+        InventoryModule,
+    ],
     controllers: [],
     providers: [],
 })
